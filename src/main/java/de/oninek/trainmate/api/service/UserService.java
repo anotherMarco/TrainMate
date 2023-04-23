71f50e6e4536406550098acecec6d6ffc0f162c1
marco <p.ulrichmarco@gmail.com>
2023-04-23 13:42:34 +0200

    Done, to whoever merges this, good luck.

--- src/main/java/de/oninek/trainmate/api/service/UserService.java
+++ src/main/java/de/oninek/trainmate/api/service/UserService.java
@@ -3,7 +3,6 @@ package de.oninek.trainmate.api.service;
 
 import de.oninek.trainmate.api.dto.CreateUserRequest;
 import de.oninek.trainmate.api.dto.UserResponse;
-import org.springframework.http.ResponseEntity;
 
 public interface UserService {
 
