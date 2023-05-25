3c677e106fe5e91135ffddad837e401c1fffaa0a
marco <p.ulrichmarco@gmail.com>
2023-05-25 12:01:54 +0200

    yolo push

--- src/main/java/de/oninek/trainmate/api/controller/EquipmentControllerImpl.java
+++ src/main/java/de/oninek/trainmate/api/controller/EquipmentControllerImpl.java
@@ -1,15 +1,21 @@
 package de.oninek.trainmate.api.controller;
 
 import de.oninek.trainmate.api.dto.EquipmentResponse;
+import de.oninek.trainmate.api.service.EquipmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 class EquipmentControllerImpl implements EquipmentController {
+
+    private final EquipmentService service;
     @Override
     public ResponseEntity<Page<EquipmentResponse>> findMany(Pageable pageable) {
-        return null;
+        Page<EquipmentResponse> equipments = service.findMany(pageable);
+        return ResponseEntity.ok(equipments);
     }
 }
