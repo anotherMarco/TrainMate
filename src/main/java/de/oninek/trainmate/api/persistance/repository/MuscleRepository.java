9bd835bd446f7ee18772a179726dbf1a1bfce6cf
marco <p.ulrichmarco@gmail.com>
2023-05-26 07:05:28 +0200

    I don't know why. Just move on.

--- src/main/java/de/oninek/trainmate/api/persistance/repository/MuscleRepository.java
+++ src/main/java/de/oninek/trainmate/api/persistance/repository/MuscleRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MuscleRepository extends TrainMateRepository<MuscleEntity> {
     Page<MuscleEntity> findAllByMuscleGroupIdIn(Pageable pageable, List<Long> muscleGroupIds);
