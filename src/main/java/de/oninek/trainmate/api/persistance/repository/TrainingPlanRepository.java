49680fb1992bd4a38fd45e68ae6f63fc40617f03
marco <p.ulrichmarco@gmail.com>
2023-06-13 14:03:25 +0200

    Future self, please forgive me and don't hit me with the baseball bat again!

--- src/main/java/de/oninek/trainmate/api/persistance/repository/TrainingPlanRepository.java
+++ src/main/java/de/oninek/trainmate/api/persistance/repository/TrainingPlanRepository.java
@@ -1,12 +1,11 @@
 package de.oninek.trainmate.api.persistance.repository;
 
-import de.oninek.trainmate.api.exceptions.MuscleNotFoundException;
-import de.oninek.trainmate.api.persistance.entity.MuscleEntity;
+import de.oninek.trainmate.api.exceptions.TrainingPlanNotFoundException;
 import de.oninek.trainmate.api.persistance.entity.TrainingPlanEntity;
 
 public interface TrainingPlanRepository extends TrainMateRepository<TrainingPlanEntity>{
     default TrainingPlanEntity findByIdOrThrow(Long id) {
-        return findById(id).orElseThrow(() -> new MuscleNotFoundException(id));
+        return findById(id).orElseThrow(() -> new TrainingPlanNotFoundException(id));
     }
 
 }
--- src/main/java/de/oninek/trainmate/api/service/mapper/TrainingPlanEntryMapper.java
+++ src/main/java/de/oninek/trainmate/api/service/mapper/TrainingPlanEntryMapper.java
@@ -2,7 +2,6 @@ package de.oninek.trainmate.api.service.mapper;
 
 import de.oninek.trainmate.api.dto.TrainingPlanEntryRequest;
 import de.oninek.trainmate.api.dto.TrainingPlanEntryResponse;
-import de.oninek.trainmate.api.dto.TrainingPlanRequest;
 import de.oninek.trainmate.api.persistance.entity.TrainingPlanEntryEntity;
 
 import java.util.List;
