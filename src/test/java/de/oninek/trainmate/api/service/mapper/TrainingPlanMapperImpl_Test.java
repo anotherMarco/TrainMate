bd809e5df2f0c5a99ee4c35cbadfc7b77dcf4ebc
marco <p.ulrichmarco@gmail.com>
2023-06-13 15:08:44 +0200

    Fixed the fuck out of #133!

--- src/test/java/de/oninek/trainmate/api/service/mapper/TrainingPlanMapperImpl_Test.java
+++ src/test/java/de/oninek/trainmate/api/service/mapper/TrainingPlanMapperImpl_Test.java
@@ -1,9 +1,13 @@
 package de.oninek.trainmate.api.service.mapper;
 
+import de.oninek.trainmate.api.dto.TrainingPlanRequest;
 import de.oninek.trainmate.api.dto.TrainingPlanResponse;
 import de.oninek.trainmate.api.persistance.entity.TrainingPlanEntity;
+import de.oninek.trainmate.api.testutil.TrainingPlanEntryTestDataBuilder;
 import de.oninek.trainmate.api.testutil.TrainingPlanTestDataBuilder;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.Spy;
@@ -11,10 +15,10 @@ import org.mockito.Spy;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TrainingPlanMapperImpl_Test {
 
-    private TrainingPlanMapper trainingPlanMapper;
+    private TrainingPlanMapper sut;
     private TrainingPlanTestDataBuilder testDataBuilder;
 
     @Spy
@@ -24,17 +28,38 @@ class TrainingPlanMapperImpl_Test {
     void setUp() {
         testDataBuilder = new TrainingPlanTestDataBuilder();
         entryMapper = Mockito.mock(TrainingPlanEntryMapper.class);
-        trainingPlanMapper = new TrainingPlanMapperImpl(entryMapper);
+        sut = new TrainingPlanMapperImpl(entryMapper);
     }
 
     @Test
-    void test() {
+    void entityToResponse() {
         TrainingPlanEntity entity = testDataBuilder.buildEntity();
 
-        TrainingPlanResponse result = trainingPlanMapper.toResponse(entity);
+        TrainingPlanResponse result = sut.toResponse(entity);
 
         verify(entryMapper, times(1)).toResponseList(any());
         assertThat(result.id()).isEqualTo(entity.getId());
         assertThat(result.name()).isEqualTo(entity.getName());
+    }
+
+    @Test
+    void requestToEntity() {
+        TrainingPlanRequest request = testDataBuilder.buildRequest();
+
+        TrainingPlanEntity result = sut.toEntity(request);
+
+        assertThat(result.getName()).isEqualTo(request.name());
+        assertThat(result.getEntries()).isEmpty();
+    }
+
+    @Test
+    void map_entries() {
+        TrainingPlanRequest request = testDataBuilder.addEntry(new TrainingPlanEntryTestDataBuilder())
+                .addEntry(new TrainingPlanEntryTestDataBuilder().setId(2L))
+                .buildRequest();
+
+        TrainingPlanEntity result = sut.toEntity(request);
+
+        verify(entryMapper, times(1)).toEntityList(request.entries());
     }
 }
--- src/test/java/de/oninek/trainmate/api/testutil/TrainingPlanTestDataBuilder.java
+++ src/test/java/de/oninek/trainmate/api/testutil/TrainingPlanTestDataBuilder.java
@@ -1,5 +1,6 @@
 package de.oninek.trainmate.api.testutil;
 
+import de.oninek.trainmate.api.dto.TrainingPlanRequest;
 import de.oninek.trainmate.api.persistance.entity.TrainingPlanEntity;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ public class TrainingPlanTestDataBuilder {
     private Long id = 1L;
     private String name = "Legs";
     private List<TrainingPlanEntryTestDataBuilder> trainingPlanEntryBuilders = new ArrayList<>();
+
     public TrainingPlanEntity buildEntity() {
         TrainingPlanEntity entity = new TrainingPlanEntity();
         entity.setId(id);
@@ -19,5 +21,25 @@ public class TrainingPlanTestDataBuilder {
                 .toList();
         entity.setEntries(entries);
         return entity;
+    }
+
+    public TrainingPlanRequest buildRequest() {
+        var requests = trainingPlanEntryBuilders.stream().map(TrainingPlanEntryTestDataBuilder::buildRequest).toList();
+        return new TrainingPlanRequest(name, requests);
+    }
+
+    public TrainingPlanTestDataBuilder setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public TrainingPlanTestDataBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public TrainingPlanTestDataBuilder addEntry(TrainingPlanEntryTestDataBuilder entryBuilder) {
+        trainingPlanEntryBuilders.add(entryBuilder);
+        return this;
     }
 }
